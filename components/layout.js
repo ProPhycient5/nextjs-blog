2d2ec241e85deb5a2ea8c422eb8aa3e3aeb34d26
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2022-03-18 13:58:00 +0530

    small change

--- components/layout.js
+++ components/layout.js
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/better-pic.jpg"
                   className={utilStyles.borderCircle}
                   height={140}
                   width={120}
